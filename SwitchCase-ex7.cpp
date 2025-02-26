a91c236633fb145266ef279a742fd497c7da0754
Ori Ruppin <oruppin@umd.edu>
2025-02-26 19:01:53 +0000

    Ex7Ori

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,29 @@
-// add your code here
+#include <iostream>
+
+int main() {
+    int num;
+    
+    // Prompt user for input
+    std::cout << "Please enter a number: ";
+    std::cin >> num;
+
+    // Determine behavior based on number properties
+    switch (num) {
+        case 0:
+        case 2:
+        case 4:
+        case 6:
+        case 8:
+            std::cout << "The number squared is: " << num * num << std::endl;
+            break;
+        default:
+            if (num % 2 == 1) {  // Check if the number is odd
+                std::cout << "The number divided by 2 is: " << num / 2.0 << std::endl;
+            } else {
+                std::cout << "hahaha" << std::endl;
+            }
+            break;
+    }
+
+    return 0;
+}
---
+++ cin-ex6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int y = 10;  // Define an integer variable
+    std::cout << "Memory address of y: " << &y << std::endl;
+
+    // Define a pointer and assign it the address of y
+    int *x = &y;
+
+    // Print the value of y and the value stored at pointer x before modifying y
+    std::cout << "Value of y: " << y << std::endl;
+    std::cout << "Value pointed to by x (before changing y): " << *x << std::endl;
+
+    // Modify y
+    y = 20;
+
+    // Print the value of y and the value stored at pointer x after modifying y
+    std::cout << "Value of y after modification: " << y << std::endl;
+    std::cout << "Value pointed to by x (after changing y): " << *x << std::endl;
+
+    return 0;
+}
