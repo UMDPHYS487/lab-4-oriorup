61637563730f0e2e2606f9ec33b8a2f7091faa5d
Ori Ruppin <oruppin@umd.edu>
2025-02-26 19:06:21 +0000

    Ex8Ori

--- Bitwise-ex8.cpp
+++ Bitwise-ex8.cpp
@@ -1,1 +1,15 @@
-// Your code here
+#include <iostream>
+#include <bitset>
+
+int main() {
+    std::bitset<8> b{0b01110010};
+    std::cout << b << " (initial value)\n";
+
+    // Flip all bits
+    b = ~b;
+
+    std::cout << b << " (flipped value)\n";
+
+    return 0;
+}
+
